3ab114eba5341bd8accb0da508dbaa0a34673bec
MJaman786 <amanmujawar064@gmail.com>
2025-09-12 20:38:20 +0530

    functions and parameters

---
+++ Javascript part 1/ch11 Functions and Parameters/script.js
@@ -0,0 +1,10 @@
+// ✅ Function Syntax
+// function functionName(parameter_1, parameter2){
+    // code
+// }
+
+// 1. Normal function
+function addNumbers(num1, num2){
+    console.log(num1 + num2);
+}
+
