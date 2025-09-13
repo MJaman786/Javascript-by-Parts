ce944b6f04dde0cea71be257eccdc8e05374caa0
MJaman786 <amanmujawar064@gmail.com>
2025-09-13 17:46:55 +0530

    added content in  Functions and Object

--- Javascript part 1/ch12 Functions with Object/script.js
+++ Javascript part 1/ch12 Functions with Object/script.js
@@ -10,5 +10,24 @@ console.log(numbers(100, 200, 400, 600)); // [ 100, 200, 400, 600 ]
 function addNumbers(num1, num2, ...num3) {
     return num1 + num2 + num3
 }
-console.log(addNumbers(100, 200, 300, 400, 600));
+console.log(addNumbers(100, 200, 300, 400, 600)); // 300300,400,600
+
+// type 3: to fix this 300300,400,600 calculation
+function add(num1, num2, ...num3) {
+    return num1 + num2 + num3.reduce((acc, value) => acc + value, 0)
+}
+// now it is calculating correctly extra argumnets 
+console.log(add(100, 200, 300, 400)); // 1000 
+
+// ✅ 2. Passing Object as argumnets
+let user = { name: "Aman", skill: "Next js" };
+function userFunction(myObject) {
+    console.log(`Username: ${myObject.name}\nUserskill: ${myObject.skill}`);
+}
+console.log(userFunction(user))
+// Username: Aman
+// Userskill: Next js
+console.log(userFunction({ name: "Mitesh", skill: "Python" }))
+// Username: Mitesh
+// Userskill: Python
 
